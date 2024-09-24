01d3cd5f76cbd944f7e20f00d0f796a9c614097c
jonasserino <jonasserino@gmail.com>
2024-09-25 07:51:05 +0800

    fix address value styling

--- app/gallery.tsx
+++ app/gallery.tsx
@@ -93,7 +93,7 @@ const Gallery = ({ users }: GalleryProps) => {
                   </div>
                   <div className="field">
                     <FaLocationDot className="icon" />
-                    <div className="data">{`${selectedUser.address.street}, ${selectedUser.address.suite}, ${selectedUser.address.city}`}</div>
+                    <div className="value">{`${selectedUser.address.street}, ${selectedUser.address.suite}, ${selectedUser.address.city}`}</div>
                   </div>
                   <div className="field">
                     <FaPhone className="icon" />
