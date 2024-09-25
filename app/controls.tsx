cb60ba6f006568eebf28e8717d927d4fde7bdf08
jonasserino <jonasserino@gmail.com>
2024-09-25 09:41:08 +0800

    implemented sort by field and sort direction for user gallery

--- app/controls.tsx
+++ app/controls.tsx
@@ -1,36 +1,64 @@
 import Select from "react-select";
+import {
+	SortDirection,
+	SortDirectionOpt,
+	SortField,
+	SortFieldOpt,
+} from "./types/sort";
 
-const Controls = () => {
-  const fieldOptions = [
-    { label: "Name", value: "name" },
-    { label: "Company", value: "company" },
-    { label: "Email", value: "email" },
-  ];
-  const directionOptions = [
-    { label: "Ascending", value: "ascending" },
-    { label: "Descending", value: "descending" },
-  ];
+type Props = {
+	sortDirection: SortDirectionOpt | null;
+	sortField: SortFieldOpt | null;
+	onSortFieldChange: (sortFieldOpt: SortFieldOpt | null) => void;
+	onSortDirectionChange: (sortDirectionOpt: SortDirectionOpt | null) => void;
+};
 
-  return (
-    <div className="gallery-controls controls">
-      <div className="form-group group">
-        <label htmlFor="sort-field" className="label">
-          Sort Field
-        </label>
-        <Select options={fieldOptions} inputId="sort-field" className="input" />
-      </div>
-      <div className="form-group group">
-        <label htmlFor="sort-direction" className="label">
-          Sort Direction
-        </label>
-        <Select
-          options={directionOptions}
-          inputId="sort-direction"
-          className="input"
-        />
-      </div>
-    </div>
-  );
+const Controls = ({
+	sortField,
+	onSortFieldChange,
+
+	sortDirection,
+	onSortDirectionChange,
+}: Props) => {
+	const fieldOptions = [
+		{ label: "Name", value: SortField.NAME },
+		{ label: "Company", value: SortField.COMPANY },
+		{ label: "Email", value: SortField.EMAIL },
+	];
+
+	const directionOptions = [
+		{ label: "Ascending", value: SortDirection.ASC },
+		{ label: "Descending", value: SortDirection.DESC },
+	];
+
+	return (
+		<div className="gallery-controls controls">
+			<div className="form-group group">
+				<label htmlFor="sort-field" className="label">
+					Sort Field
+				</label>
+				<Select
+					value={sortField}
+					onChange={(v) => onSortFieldChange(v)}
+					options={fieldOptions}
+					inputId="sort-field"
+					className="input"
+				/>
+			</div>
+			<div className="form-group group">
+				<label htmlFor="sort-direction" className="label">
+					Sort Direction
+				</label>
+				<Select
+					value={sortDirection}
+					onChange={(v) => onSortDirectionChange(v)}
+					options={directionOptions}
+					inputId="sort-direction"
+					className="input"
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Controls;
