dd154c3479697849b8a76044c44a3356149521b3
jonasserino <jonasserino@gmail.com>
2024-09-25 11:20:13 +0800

    add getUsers api function

---
+++ app/lib/users.ts
@@ -0,0 +1,8 @@
+import { USER_API_HOST } from "../constants";
+
+export async function getUsers() {
+	const response = await fetch(`${USER_API_HOST}/users`);
+	const users = await response.json();
+
+	return users;
+}
