b02b853af510dbf14989cb9c3c510dafe7327b5b
jonasserino <jonasserino@gmail.com>
2024-09-26 13:54:00 +0800

    change user list html elements to semantic elements

--- components/users/gallery.tsx
+++ components/users/gallery.tsx
@@ -39,9 +39,9 @@ const Gallery = ({ users }: GalleryProps) => {
 	return (
 		<div className="user-gallery">
 			<h1 className="heading">Users</h1>
-			<div className="items">
+			<ul className="items">
 				{usersList.map((user, index) => (
-					<div
+					<li
 						className="item user-card"
 						key={index}
 						onClick={() => handleModalOpen(user.id)}
@@ -58,80 +58,80 @@ const Gallery = ({ users }: GalleryProps) => {
 							<div className="name">{user.name}</div>
 							<div className="company">{user.company.name}</div>
 						</div>
-					</div>
+					</li>
 				))}
-				<Modal isOpen={isModalOpen} onClose={handleModalClose}>
-					<div className="user-panel">
-						<div className="header">
-							<div
-								role="button"
-								tabIndex={0}
-								className="close"
-								onClick={handleModalClose}
-							>
-								<FaRegCircleXmark size={32} />
-							</div>
-						</div>
-						<div className="body">
-							{selectedUser && (
-								<dl className="user-info info">
-									<div className="avatar">
-										<Avatar
-											size={240}
-											name={selectedUser.name}
-											variant="marble"
-											colors={[
-												"#92A1C6",
-												"#146A7C",
-												"#F0AB3D",
-												"#C271B4",
-												"#C20D90",
-											]}
-										/>
-									</div>
-									<div className="name">
-										<dt className="sr-only">Name - Username</dt>
-										<dd>
-											{selectedUser.name} ({selectedUser.username})
-										</dd>
-									</div>
-									<div className="field">
-										<dt className="sr-only">Address</dt>
-										<FaLocationDot className="icon" />
-										<dd className="value">{`${selectedUser.address.street}, ${selectedUser.address.suite}, ${selectedUser.address.city}`}</dd>
-									</div>
-									<div className="field">
-										<dt className="sr-only">Phone</dt>
-										<FaPhone className="icon" />
-										<dd className="value">
-											<Link href={`tel:${selectedUser.phone}`}>
-												{selectedUser.phone}
-											</Link>
-										</dd>
-									</div>
-									<div className="field">
-										<dt className="sr-only">Email</dt>
-										<FaEnvelope className="icon" />
-										<dd className="value">
-											<Link href={`mailto:${selectedUser.email}`}>
-												{selectedUser.email}
-											</Link>
-										</dd>
-									</div>
-									<div className="company">
-										<dt className="sr-only">Company</dt>
-										<dd className="name">{selectedUser.company.name}</dd>
-										<dt className="sr-only">Company Catch Phrase</dt>
-										<div className="catchphrase">
-											{selectedUser.company.catchPhrase}
-										</div>
-									</div>
-								</dl>
-							)}
+			</ul>
+			<Modal isOpen={isModalOpen} onClose={handleModalClose}>
+				<div className="user-panel">
+					<div className="header">
+						<div
+							role="button"
+							tabIndex={0}
+							className="close"
+							onClick={handleModalClose}
+						>
+							<FaRegCircleXmark size={32} />
 						</div>
 					</div>
-				</Modal>
-			</div>
+					<div className="body">
+						{selectedUser && (
+							<dl className="user-info info">
+								<div className="avatar">
+									<Avatar
+										size={240}
+										name={selectedUser.name}
+										variant="marble"
+										colors={[
+											"#92A1C6",
+											"#146A7C",
+											"#F0AB3D",
+											"#C271B4",
+											"#C20D90",
+										]}
+									/>
+								</div>
+								<div className="name">
+									<dt className="sr-only">Name - Username</dt>
+									<dd>
+										{selectedUser.name} ({selectedUser.username})
+									</dd>
+								</div>
+								<div className="field">
+									<dt className="sr-only">Address</dt>
+									<FaLocationDot className="icon" />
+									<dd className="value">{`${selectedUser.address.street}, ${selectedUser.address.suite}, ${selectedUser.address.city}`}</dd>
+								</div>
+								<div className="field">
+									<dt className="sr-only">Phone</dt>
+									<FaPhone className="icon" />
+									<dd className="value">
+										<Link href={`tel:${selectedUser.phone}`}>
+											{selectedUser.phone}
+										</Link>
+									</dd>
+								</div>
+								<div className="field">
+									<dt className="sr-only">Email</dt>
+									<FaEnvelope className="icon" />
+									<dd className="value">
+										<Link href={`mailto:${selectedUser.email}`}>
+											{selectedUser.email}
+										</Link>
+									</dd>
+								</div>
+								<div className="company">
+									<dt className="sr-only">Company</dt>
+									<dd className="name">{selectedUser.company.name}</dd>
+									<dt className="sr-only">Company Catch Phrase</dt>
+									<div className="catchphrase">
+										{selectedUser.company.catchPhrase}
+									</div>
+								</div>
+							</dl>
+						)}
+					</div>
+				</div>
+			</Modal>
 		</div>
 	);
 };
